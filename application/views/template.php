4189f0760c630ac3d7b3655e7deb008c4071cf4f
Rystraum Gamonez <rystraum@gmail.com>
2011-09-04 23:52:08 +0800

    added basic skeleton template

--- application/views/template.php
+++ application/views/template.php
@@ -9,7 +9,32 @@
     ?>
 </head>
 <body>
-<?php print $contents ?>
+    <div id="container">
+        <div id="topbar">
+            <?php
+                if ($this->tank_auth->is_logged_in()):
+                    print anchor('user',$this->tank_auth->get_username());
+                    print anchor('auth/logout','Logout');
+                else:
+                    print anchor('auth/login','Login');
+                endif;
+            ?>
+        </div>
+        <div id="header">
+            <img src="<?php echo base_url('public/images/logo.png') ?>" alt="logo" id="logo" />
+            <ul id="nav">
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div id="content">
+            <?php print $contents ?>
+        </div>
+        <div id="footer">
+        
+        </div>
+    </div>
 </body>
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
 </html>
