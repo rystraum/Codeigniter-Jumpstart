a8730e78b748616e14c2d27971c70eb7744a6658
Rystraum Gamonez <rystraum@gmail.com>
2011-09-05 10:42:17 +0800

    added checking if user is logged in in user controller, unsetting password

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -3,8 +3,10 @@
 class User extends MY_Controller {
     function __construct() {
         parent::__construct();
+        if (!$this->tank_auth->is_logged_in()) redirect('/auth/login/');
         $this->load->model('profiles');
         $this->data['user'] = $this->profiles->get_user_by_id($this->tank_auth->get_user_id(), TRUE);
+        unset($this->data['user']['password']);
     }
     
     function index() {
