4d286d7bcbb7f0974516c034cc6f09bdba260880
Rystraum Gamonez <rystraum@gmail.com>
2011-09-05 09:59:30 +0800

    email config

--- application/config/email.php
+++ application/config/email.php
@@ -10,10 +10,16 @@
 |	http://codeigniter.com/user_guide/libraries/email.html
 |
 */
-$config['mailtype'] = 'html';
-$config['charset'] = 'utf-8';
-$config['newline'] = "\r\n";
-
+$config = array(
+    'protocol' => 'smtp',
+    'smtp_host' => 'ssl://smtp.googlemail.com',
+    'smtp_port' => 465,
+    'smtp_user' => 'email@personal-it-y.com',
+    'smtp_pass' => 'password',
+    'mailtype' => 'html',
+    'charset'  => 'utf-8',
+    'newline'  => "\r\n",
+);
 
 /* End of file email.php */
 /* Location: ./application/config/email.php */
