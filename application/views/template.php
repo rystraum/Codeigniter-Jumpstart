442255a695e0eba9bdf13cde1e68a2d804f6fc93
Rystraum Gamonez <rystraum@gmail.com>
2011-09-08 01:00:30 +0800

    added 1140 css from http://cssgrid.net/

--- application/views/template.php
+++ application/views/template.php
@@ -6,32 +6,55 @@
     <?php
         echo link_tag('public/reset.css');
         echo link_tag('public/defaults.css');
+        echo link_tag('public/css/1140.css');
+        echo link_tag('public/css/ie.css');
+        echo link_tag('public/css/styles.css');
     ?>
 </head>
 <body>
     <div id="container">
-        <div id="topbar">
-            <?php
-                if ($this->tank_auth->is_logged_in()):
-                    print "Welcome, " . anchor('user',$this->tank_auth->get_username());
-                    print anchor('auth/logout','Logout');
-                else:
-                    print anchor('auth/login','Login');
-                endif;
-            ?>
+        <div id="topbar" class="container">
+            <div class="row">
+                <div class="twelvecol last">
+                    <?php
+                        if ($this->tank_auth->is_logged_in()):
+                            print "Welcome, " . anchor('user',$this->tank_auth->get_username());
+                            print anchor('auth/logout','Logout');
+                        else:
+                            print anchor('auth/login','Login');
+                        endif;
+                    ?>
+                </div>
+            </div>
         </div>
-        <div id="header">
-            <a href="<?php base_url() ?>"><img src="<?php echo base_url('public/images/logo.png') ?>" alt="logo" id="logo" /></a>
-            <ul id="nav">
-                <li><?php echo anchor('','Home') ?></li>
-                <li><?php echo anchor('contact_us','Contact Us') ?></li>
-            </ul>
+        <div id="header" class="container">
+            <div class="row">
+                <div class="threecol logo-container">
+                    <a href="<?php base_url() ?>">
+                        <img src="<?php echo base_url('public/images/logo.png') ?>" alt="logo" id="logo" />
+                    </a>
+                </div>
+                <div class="ninecol last">
+                    <ul id="nav">
+                        <li><?php echo anchor('','Home') ?></li>
+                        <li><?php echo anchor('contact_us','Contact Us') ?></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div id="content">
-            <?php print $contents ?>
+        <div id="content" class="container">
+            <div class="row">
+                <div class="twelvecol last">
+                    <?php print $contents ?>                
+                </div>
+            </div>
         </div>
-        <div id="footer">
-        
+        <div id="footer" class="container">
+            <div class="row">
+                <div class="twelvecol last">
+                
+                </div>
+            </div>
         </div>
     </div>
 </body>
