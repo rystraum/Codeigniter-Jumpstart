e19e56573a93ae8c1d8316b2b32d8eb757bb3954
Rystraum Gamonez <rystraum@gmail.com>
2011-10-14 18:06:21 +0800

    added styling for user profiles

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -1,6 +1,6 @@
-<dl>
+<dl id="user-profiles">
 <?php foreach ($user as $k => $v): ?>
     <dt><?php print $k ?></dt>
-    <dd><?php print $v ?></dd>
+    <dd><?php print ($v)? $v : '-' ?></dd>
 <?php endforeach ?>
 </dl>
